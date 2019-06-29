01c363483944607093aaddf1d960e236b8342460
shivamag437 <shivamag437@gmail.com>
2019-06-29 15:51:49 +0530

    Padding file updated

--- padding.h
+++ padding.h
@@ -9,10 +9,12 @@ void pad(char* plaintext){
 	printf("length: %d\n", length);
 
 	pad_value = 16 - length%16;
+	printf("pad_value: %d\n",pad_value);
 	if(pad_value == 0){
 		padded = (char *)malloc((length+17)*sizeof(char));
 		strcpy(padded, plaintext);
 		strcat(padded, "0000000000000000");
+		printf("padded: %s\n",padded);
 		plaintext=(char *)realloc(padded,(length+17)*sizeof(char));
 	}
 
@@ -20,9 +22,10 @@ void pad(char* plaintext){
 		padded = (char *)malloc((length+pad_value+1)*sizeof(char));
 		strcpy(padded, plaintext);
 		for(int i=0 ; i < pad_value; i++){
-			padded[length+i] = (char)pad_value;
+			padded[length+i] = (char)(pad_value+64);
 		}
 		padded[length+pad_value] = '\0';
+		printf("padded: %s\n",padded);
 		//printf("%s\n", padded);
 		plaintext=(char *)realloc(padded,(length+pad_value+1)*sizeof(char));
 	}
@@ -33,7 +36,7 @@ void de_pad(char* plaintext){
 	char* padded;
 	length = strlen(plaintext);
 
-	pad_value = plaintext[length-1];
+	pad_value = plaintext[length-1]-64;
 	if(pad_value == 0){
 		padded =  (char *)realloc(plaintext, (length-15)*sizeof(char));
 	}
