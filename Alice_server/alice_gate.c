4f11b90712b562ea387e081c114f382b74f2d06d
Prashanthi <prashiram99@yahoo.co.in>
2019-07-21 15:59:59 +0530

    updated headerfiles

--- Alice_server/alice_gate.c
+++ Alice_server/alice_gate.c
@@ -3,10 +3,10 @@
 #include<stdlib.h> 
 #include<stdint.h>
 #include<string.h>
-#include "aes.h"
-#include "aes.c"
-#include "padding.h"
-#include "sha3.c"
+#include "../aes.h"
+#include "../aes.c"
+#include "../padding.h"
+#include "../sha3.c"
 
 #define KEY_LENGTH 128
 #define IV_LENGTH 16
